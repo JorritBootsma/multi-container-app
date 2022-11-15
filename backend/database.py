9dde8890475be2dc508bbdb8ee8a5873d1cba397
Jorrit Bootsma <jorrit@Jorrits-MacBook-Pro.local>
2022-11-15 15:07:37 +0100

    removed creds from source code

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -6,32 +8,14 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 Base = declarative_base()
 
 # Database connection specifics for local Docker SQL Edge Database
-server = "db"
-user = "dummy_user"
-password = "dummy_password"
-database = "dummy_db"
+server = os.environ["DB_SERVER"]
+user = os.environ["DB_USER"]
+password = os.environ["DB_PASSWORD"]
+database = os.environ["DB_NAME"]
 conn_string = f"postgresql+psycopg2://{user}:{password}@{server}/{database}"
-print(conn_string)
 
 # Create engine
 engine = create_engine(conn_string)
 
 # Initialise session class
 Session = sessionmaker()
-
-# all_tables = engine.execute(
-#     """
-#              SELECT
-#                  table_schema || '.' || table_name
-#              FROM
-#                  information_schema.tables
-#              WHERE
-#                  table_type = 'BASE TABLE'
-#              AND
-#                  table_schema NOT IN ('pg_catalog', 'information_schema');
-#              """
-# ).fetchall()
-#
-# print(len(all_tables))
-# table_names = [tables_info[0] for tables_info in all_tables]
-# print(table_names)
